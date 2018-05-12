d424fbf8179c2ba64f91b0ca3b3dfc973642d9b8
cyreal123@gmail.com <cyreal123@gmail.com>
2018-05-12 13:42:30 -0400

    Updated Node attribute and removed impl

--- converted/ofbx/nodeAttribute.go
+++ converted/ofbx/nodeAttribute.go
@@ -2,6 +2,7 @@ package ofbx
 
 type NodeAttribute struct {
 	Object
+	attribute_type DataView
 }
 
 func NewNodeAttribute(scene *Scene, element *IElement) *NodeAttribute {
@@ -13,41 +14,19 @@ func (na *NodeAttribute) Type() Type {
 }
 
 func (na NodeAttribute) getAttributeType() DataView {
-	return DataView{}
+	return na.attribute_type
 }
 
-
+func parseNodeAttribute(scene *Scene, element *IElement) *NodeAttribute, Error{
+	na := NewNodeAttribute(scene, element)
+	type_flags := findChild(element, "TypeFlags")
+	if (type_flags != nil  && type_flags.first_property)	{
+		na.attribute_type = type_flags.first_property.value;
+	}
+	return na, nil
+}
 
 // From CPP
-
-
-
-static OptionalError<Object*> parseNodeAttribute(const Scene& scene, const Element& element)
-{
-	NodeAttributeImpl* obj = new NodeAttributeImpl(scene, element);
-	const Element* type_flags = findChild(element, "TypeFlags");
-	if (type_flags && type_flags.first_property)
-	{
-		obj.attribute_type = type_flags.first_property.value;
-	}
-	return obj;
+func (na *NodeAttribute) getType() Type{
+	return na.Type()
 }
-
-NodeAttribute::NodeAttribute(const Scene& _scene, const IElement& _element)
-	: Object(_scene, _element)
-{
-}
-
-
-struct NodeAttributeImpl : NodeAttribute
-{
-	NodeAttributeImpl(const Scene& _scene, const IElement& _element)
-		: NodeAttribute(_scene, _element)
-	{
-	}
-	Type getType() const override { return Type::NODE_ATTRIBUTE; }
-	DataView getAttributeType() const override { return attribute_type; }
-
-
-	DataView attribute_type;
-};
