76c3e2e5d712c19cfbfb2a5f8bdc1115768ec9ef
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-05-12 11:47:13 -0500

    Update scene to go

--- converted/ofbx/scene.go
+++ converted/ofbx/scene.go
@@ -1,141 +1,101 @@
 package ofbx
 
-type Scene struct{}
-struct Scene : IScene
-{
-	struct Connection
-	{
-		enum Type
-		{
-			OBJECT_OBJECT,
-			OBJECT_PROPERTY
-		};
+type Connection struct {
+	ConnectionType
+	typ Type
+	from, to uint64
+	property DataView
+}
 
-		Type type;
-		uint64 from;
-		uint64 to;
-		DataView property;
-	};
+type ConnectionType int
 
-	struct ObjectPair
-	{
-		const Element* element;
-		Object* object;
-	};
+// Connection Types
+const (
+	OBJECT_OBJECT ConnectionType
+	OBJECT_PROPERTY ConnectionType
+)
 
+type ObjectPair struct {
+	element *Element
+	object *Object
+}
+	
+//const GlobalSettings* getGlobalSettings() const override { return &m_settings; }
 
-	int getAnimationStackCount() const override { return (int)m_animation_stacks.size(); }
-	int getMeshCount() const override { return (int)m_meshes.size(); }
-	float getSceneFrameRate() const override { return m_scene_frame_rate; }
-	const GlobalSettings* getGlobalSettings() const override { return &m_settings; }
+type Scene struct{
+	m_root_element *Element 
+	m_root *Root
+	m_scene_frame_rate float32 // = -1
+	m_settings GlobalSettings
+	m_object_map map[uint64]ObjectPair
+	m_all_objects []*Object
+	m_meshes []*Mesh
+	m_animation_stacks []*AnimationStack
+	m_connections []Connection
+	m_data []byte
+	m_take_infos []TakeInfo
+}
 
-	const Object* const* getAllObjects() const override { return m_all_objects.empty() ? nullptr : &m_all_objects[0]; }
-
-
-	int getAllObjectCount() const override { return (int)m_all_objects.size(); }
-
-
-	const AnimationStack* getAnimationStack(int index) const override
-	{
-		assert(index >= 0);
-		assert(index < m_animation_stacks.size());
-		return m_animation_stacks[index];
-	}
-
-
-	const Mesh* getMesh(int index) const override
-	{
-		assert(index >= 0);
-		assert(index < m_meshes.size());
-		return m_meshes[index];
-	}
-
-
-	const TakeInfo* getTakeInfo(const char* name) const override
-	{
-		for (const TakeInfo& info : m_take_infos)
-		{
-			if (info.name == name) return &info;
+func (s *Scene) getRootElement() *Element {
+	return s.m_root_element
+}
+func (s *Scene) getRoot() *Object {
+	return s.m_root
+}
+func (s *Scene) getTakeInfo(name string) *TakeInfo {
+	for _, info := range m_take_infos {
+		if info.name == name {
+			return &info
 		}
-		return nullptr;
 	}
+	return nil
+}
+func (s *Scene) getSceneFrameRate() float32 {
+	return s.m_scene_frame_rate
+}
+func (s *Scene) getMesh(int index) *Mesh {
+	//assert(index >= 0);
+	//assert(index < m_meshes.size());
+	return m_meshes[index]
+}
+func (s *Scene) getAnimationStack(index int) *AnimationStack {
+	//assert(index >= 0);
+	//assert(index < m_animation_stacks.size());
+	return m_animation_stacks[index]
+	
+}
+func (s *Scene) getAllObjects() []Object {
+	return s.m_all_objects
+}
 
+func load(data []byte) *Scene {
+	s := &Scene{}
+	s.m_data = make([]byte, len(data))
+	copy(s.m_data, data)
 
-	const IElement* getRootElement() const override { return m_root_element; }
-	const Object* getRoot() const override { return m_root; }
-
-
-	void destroy() override { delete this; }
-
-
-	~Scene()
-	{
-		for (auto iter : m_object_map)
-		{
-			delete iter.second.object;
+	root, err := tokenize(s.m_data) 
+	if err != nil {
+		root, err = tokenizeText(s.m_data)
+		if err != nil {
+			return nil
 		}
-		
-		deleteElement(m_root_element);
 	}
 
+	scene.m_root_element = root.getValue()
+	//assert(scene.m_root_element);
 
-	Element* m_root_element = nullptr;
-	Root* m_root = nullptr;
-	float m_scene_frame_rate = -1;
-	GlobalSettings m_settings;
-	std::unordered_map<uint64, ObjectPair> m_object_map;
-	std::vector<Object*> m_all_objects;
-	std::vector<Mesh*> m_meshes;
-	std::vector<AnimationStack*> m_animation_stacks;
-	std::vector<Connection> m_connections;
-	std::vector<uint8> m_data;
-	std::vector<TakeInfo> m_take_infos;
-};
-
-type IScene struct{}
-
-func (is *IScene) getRootElement() *IElement {
-	return nil
-}
-func (is *IScene) getRoot() *Object {
-	return nil
-}
-func (is *IScene) getTakeInfo(name string) *TakeInfo {
-	return nil
-}
-func (is *IScene) getSceneFrameRate() float32 {
-	return 0
-}
-func (is *IScene) getMesh(int index) []Mesh {
-	return nil
-}
-func (is *IScene) getAnimationStack(index int) []AnimationStack {
-	return nil
-}
-func (is *IScene) getAllObjects() []Object {
-	return nil
-}
-
-func load(data []byte) *Iscene {
-	std::unique_ptr<Scene> scene = std::make_unique<Scene>();
-	scene.m_data.resize(size);
-	memcpy(&scene.m_data[0], data, size);
-	OptionalError<Element*> root = tokenize(&scene.m_data[0], size);
-	if (root.isError())
-	{
-		Error::s_message = "";
-		root = tokenizeText(&scene.m_data[0], size);
-		if (root.isError()) return nullptr;
+	// This was commented out already I didn't do it
+	//if (parseTemplates(*root.getValue()).isError()) return nil
+	if !parseConnections(*root.getValue(), scene.get()) {
+		return nil
 	}
+	if !parseTakes(scene.get()) {
+		return nil
+	}
+	if !parseObjects(*root.getValue(), scene.get()) {
+		return nil
+	}
+	parseGlobalSettings(*root.getValue(), scene.get())
 
-	scene.m_root_element = root.getValue();
-	assert(scene.m_root_element);
-
-	//if (parseTemplates(*root.getValue()).isError()) return nullptr;
-	if(!parseConnections(*root.getValue(), scene.get())) return nullptr;
-	if(!parseTakes(scene.get())) return nullptr;
-	if(!parseObjects(*root.getValue(), scene.get())) return nullptr;
-	parseGlobalSettings(*root.getValue(), scene.get());
-
-	return scene.release();
+	return scene.release()
 }
