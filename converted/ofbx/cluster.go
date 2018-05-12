f10fc542011e88449834561720d6287d8bc20d4a
cyreal123@gmail.com <cyreal123@gmail.com>
2018-05-12 13:34:50 -0400

    Updated cluster

--- converted/ofbx/cluster.go
+++ converted/ofbx/cluster.go
@@ -2,137 +2,141 @@ package ofbx
 
 type Cluster struct {
 	Object
+
+	 link  *Object
+	 skin *Skin
+	 indices []int
+	 weights []float64
+	transform_matrix Matrix
+	transform_link_matrix Matrix
 }
 
 func NewCluster(scene *Scene, element *IElement) *Cluster {
-	return nil
+
+
+
+
+	c := NewObject(scene, element)
+	c.clusterPostProcess(scene, element)
+	c.link = &Object{}
+	c.skin := &Skin{}
+	c.transform_link_matrix = NewMatrix(scene, element) 
+	c.transform_matrix= NewMatrix(scene, element)
+
+	return c
 }
 
 func (c *Cluster) Type() Type {
 	return CLUSTER
 }
 
+//TODO: should this really just be a link to the first index? as  pointer?
+//const int* getIndices() const override { return &indices[0]; }
 func (c *Cluster) getIndices() []int {
 	return nil
+	
+}
+func (c *Cluster) getIndicesCount() int{
+	return len(getIndices)
 }
 
 func (c *Cluster) getWeights() *float64 {
-	return nil
+	return c.weights
+}
+func (c *Cluster) getWeightsCount() int{
+	return len(c.weights)
 }
 
 func (c *Cluster) getTransformMatrix() Matrix {
-	return Matrix{}
+	return c.transform_matrix
 }
 
 func (c *Cluster) getTransformLinkMatrix() Matrix {
-	return Matrix{}
+	return c.transform_link_matrix
 }
 
 func (c *Cluster) getLink() *Object {
-	return nil
+	return c.link
+}
+
+
+// clusterPostProcess adds the additional fields that clusters have over just object fields.
+// In this case its setting up indicies and weights
+func (c *Cluster) clusterPostProcess(scene, element){
+	 geom := (*GeometryImpl).skin.resolveObjectLinkReverse(GEOMETRY)
+	 if (geom==nil){
+		return false
+  	 }
+	   old_indices := []int{}
+	   indexes := findChild(element, "Indexes");
+	   if (indexes != nil && indexes.first_property)   {
+		   if (!parseBinaryArray(*indexes.first_property, &old_indices)){ 
+			   return false
+			}
+	   }
+	   old_weights := []float64
+	   weights_el := findChild(element, "Weights");
+   		if (weights_el != nil && weights_el.first_property)	{
+			if (!parseBinaryArray(*weights_el.first_property, &old_weights)){
+				 return false
+			}
+		}
+
+		iLen := len(old_indices)
+		if (iLen != len(old_weights)){
+			return false
+		}
+
+		c.weights := make([]float64)
+		c.indices := make([]int)
+
+
+		for(i := 0; i < iLen; i++){
+			n := &geom.to_new_vertices[old_idx] //was a geometryimpl NewVertex
+			if (n.index == -1) continue; // skip vertices which aren't indexed.
+			for (n != nil)
+			{
+				c.indices[i]
+				c.indices = c.indices.append(n.index)
+				c.weights = c.weights.append(w);
+				n = n.next;
+			}
+		}
+		return true
+
+}
+
+
+func parseCluster(scene *Scene, element *IElement) *Object, Error{
+	cluster := NewCLuster(scene, element)
+	cluster.transform_link = findChild(element, "TransformLink");
+	if (transform_link !=nil && transform_link.first_property)
+	{
+		if (!parseArrayRaw(
+				*transform_link.first_property, &obj.transform_link_matrix, sizeof(obj.transform_link_matrix)))
+		{
+			return nil, errors.New("Failed to parse TransformLink");
+		}
+	}
+	cluster.transform = findChild(element, "Transform");
+	if (transform!=nil && transform.first_property)
+	{
+		if (!parseArrayRaw(*transform.first_property, &obj.transform_matrix, sizeof(obj.transform_matrix)))
+		{
+			return nil, errors.New("Failed to parse Transform");
+
+		}
+	}	
+	return cluster, nil
 }
 
 
 //from cpp
 
-struct ClusterImpl : Cluster
-{
-	ClusterImpl(const Scene& _scene, const IElement& _element)
-		: Cluster(_scene, _element)
-	{
-	}
-
-	const int* getIndices() const override { return &indices[0]; }
-	int getIndicesCount() const override { return (int)indices.size(); }
-	const double* getWeights() const override { return &weights[0]; }
-	int getWeightsCount() const override { return (int)weights.size(); }
-	Matrix getTransformMatrix() const override { return transform_matrix; }
-	Matrix getTransformLinkMatrix() const override { return transform_link_matrix; }
-	Object* getLink() const override { return link; }
-
-
-	bool postprocess()
-	{
-		assert(skin);
-
-		GeometryImpl* geom = (GeometryImpl*)skin.resolveObjectLinkReverse(Object::Type::GEOMETRY);
-		if (!geom) return false;
-
-		std::vector<int> old_indices;
-		const Element* indexes = findChild((const Element&)element, "Indexes");
-		if (indexes && indexes.first_property)
-		{
-			if (!parseBinaryArray(*indexes.first_property, &old_indices)) return false;
-		}
-
-		std::vector<double> old_weights;
-		const Element* weights_el = findChild((const Element&)element, "Weights");
-		if (weights_el && weights_el.first_property)
-		{
-			if (!parseBinaryArray(*weights_el.first_property, &old_weights)) return false;
-		}
-
-		if (old_indices.size() != old_weights.size()) return false;
-
-		indices.reserve(old_indices.size());
-		weights.reserve(old_indices.size());
-		int* ir = old_indices.empty() ? nullptr : &old_indices[0];
-		double* wr = old_weights.empty() ? nullptr : &old_weights[0];
-		for (int i = 0, c = (int)old_indices.size(); i < c; ++i)
-		{
-			int old_idx = ir[i];
-			double w = wr[i];
-			GeometryImpl::NewVertex* n = &geom.to_new_vertices[old_idx];
-			if (n.index == -1) continue; // skip vertices which aren't indexed.
-			while (n)
-			{
-				indices.push_back(n.index);
-				weights.push_back(w);
-				n = n.next;
-			}
-		}
-
-		return true;
-	}
-
-
-	Object* link = nullptr;
-	Skin* skin = nullptr;
-	std::vector<int> indices;
-	std::vector<double> weights;
-	Matrix transform_matrix;
-	Matrix transform_link_matrix;
-	Type getType() const override { return Type::CLUSTER; }
-};
-
-Cluster::Cluster(const Scene& _scene, const IElement& _element)
-	: Object(_scene, _element)
-{
+func (c *Cluster) getType() Type{
+	return c.Type()
 }
 
 
-static OptionalError<Object*> parseCluster(const Scene& scene, const Element& element)
-{
-	std::unique_ptr<ClusterImpl> obj = std::make_unique<ClusterImpl>(scene, element);
 
-	const Element* transform_link = findChild(element, "TransformLink");
-	if (transform_link && transform_link.first_property)
-	{
-		if (!parseArrayRaw(
-				*transform_link.first_property, &obj.transform_link_matrix, sizeof(obj.transform_link_matrix)))
-		{
-			return Error("Failed to parse TransformLink");
-		}
-	}
-	const Element* transform = findChild(element, "Transform");
-	if (transform && transform.first_property)
-	{
-		if (!parseArrayRaw(*transform.first_property, &obj.transform_matrix, sizeof(obj.transform_matrix)))
-		{
-			return Error("Failed to parse Transform");
 
-		}
-	}
-
-	return obj.release();
-}
