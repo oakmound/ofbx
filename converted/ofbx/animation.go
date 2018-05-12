e299b78f04b8d21b26aebeaa6cdd644bbcd68024
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-05-12 12:09:54 -0500

    Converting animation and animationCurve

--- converted/ofbx/animation.go
+++ converted/ofbx/animation.go
@@ -5,23 +5,25 @@ type AnimationStack struct {
 }
 
 func NewAnimationStack(scene *Scene, element *IElement) *AnimationStack {
-	return nil
+	return &AnimationStack{}
 }
 
 func (as *AnimationStack) Type() Type {
 	return ANIMATION_STACK
 }
 
-func (as *AnimationStack) getLayer() int {
-	return 0
+func (as *AnimationStack) getLayer(index int) *AnimationLayer {
+	// This may need to change
+	return as.resolveObjectLink(index)
 }
 
 type AnimationLayer struct {
 	Object
+	curve_nodes []*AnimationCurveNode
 }
 
 func NewAnimationLayer(scene *Scene, element *IElement) *AnimationLayer {
-	return nil
+	return &AnimationLayer{}
 }
 
 func (as *AnimationLayer) Type() Type {
@@ -29,70 +31,14 @@ func (as *AnimationLayer) Type() Type {
 }
 
 func (as *AnimationLayer) getCurveNodeIndex(index int) *AnimationCurveNode {
-	return 0
+	return as.curve_nodes[index]
 }
 
 func (as *AnimationLayer) getCurveNodeIndex(bone *Object, property string) *AnimationCurveNode {
-	return 0
-}
-
-
-AnimationStack::AnimationStack(const Scene& _scene, const IElement& _element)
-	: Object(_scene, _element)
-{
-}
-
-
-AnimationLayer::AnimationLayer(const Scene& _scene, const IElement& _element)
-	: Object(_scene, _element)
-{
-}
-
-
-struct AnimationStackImpl : AnimationStack
-{
-	AnimationStackImpl(const Scene& _scene, const IElement& _element)
-		: AnimationStack(_scene, _element)
-	{
-	}
-
-
-	const AnimationLayer* getLayer(int index) const override
-	{
-		return resolveObjectLink<AnimationLayer>(index);
-	}
-
-
-	Type getType() const override { return Type::ANIMATION_STACK; }
-};
-
-struct AnimationLayerImpl : AnimationLayer
-{
-	AnimationLayerImpl(const Scene& _scene, const IElement& _element)
-		: AnimationLayer(_scene, _element)
-	{
-	}
-
-
-	Type getType() const override { return Type::ANIMATION_LAYER; }
-
-
-	const AnimationCurveNode* getCurveNode(int index) const override
-	{
-		if (index >= curve_nodes.size() || index < 0) return nullptr;
-		return curve_nodes[index];
-	}
-
-
-	const AnimationCurveNode* getCurveNode(const Object& bone, const char* prop) const override
-	{
-		for (const AnimationCurveNodeImpl* node : curve_nodes)
-		{
-			if (node.bone_link_property == prop && node.bone == &bone) return node;
+	for _, node := range as.curve_nodes {
+		if node.bone_link_property == prop && node.bone == &bone {
+			return node
 		}
-		return nullptr;
 	}
-
-
-	std::vector<AnimationCurveNodeImpl*> curve_nodes;
-};
+	return nil
+}
--- converted/ofbx/animationCurve.go
+++ converted/ofbx/animationCurve.go
@@ -1,32 +1,46 @@
 package ofbx
 
+type CurveMode int
+
+const (
+	TRANSLATION CurveMode = iota
+	ROTATION    CurveMode = iota
+	SCALE       CurveMode = iota
+)
+
 type AnimationCurve struct {
 	Object
+	times  []int64
+	values []float32
 }
 
 func NewAnimationCurve(scene *Scene, element *IElement) *AnimationCurve {
-	return nil
+	return &AnimationCurve{}
 }
 
 func (ac *AnimationCurve) Type() Type {
 	return ANIMATION_CURVE
 }
 
-// 200sc note: this may be the length of the next two functions, i.e. unneeded
-func (ac *AnimationCurve) getKeyCount() int {
-	return 0
+func (ac *AnimationCurve) getKeyTime() []int64 {
+	return ac.times
 }
 
-func (ac *AnimationCurve) getKeyTime() *int64 {
-	return nil
-}
-
-func (ac *AnimationCurve) getKeyValue() *float32 {
-	return nil
+func (ac *AnimationCurve) getKeyValue() []float32 {
+	return ac.values
 }
 
 type AnimationCurveNode struct {
 	Object
+	curves             [3]Curve
+	bone               *Object
+	bone_link_property DataView
+	mode               CurveMode
+}
+
+type Curve struct {
+	curve      *AnimationCurve
+	connection *Connection
 }
 
 func (acn *AnimationCurveNode) Type() Type {
@@ -34,87 +48,39 @@ func (acn *AnimationCurveNode) Type() Type {
 }
 
 func (acn *AnimationCurveNode) getNodeLocalTransform(time float64) Vec3 {
-	return Vec3{}
+	fbx_time := secondsToFbxTime(time)
+
+	getCoord := func(curve *Curve, fbx_time int64) float32 {
+		if curve.curve == nil {
+			return 0.0
+		}
+
+		times := curve.curve.getKeyTime()
+		values := curve.curve.getKeyValue()
+		count := len(times)
+
+		if fbx_time < times[0] {
+			fbx_time = times[0]
+		}
+		if fbx_time > times[count-1] {
+			fbx_time = times[count-1]
+		}
+		for i := 1; i < count; i++ {
+			if times[i] >= fbx_time {
+				t := float(float64(fbx_time-times[i-1]) / float64(times[i]-times[i-1]))
+				return values[i-1]*(1-t) + values[i]*t
+			}
+		}
+		return values[0]
+	}
+
+	return Vec3{
+		getCoord(acn.curves[0], fbx_time),
+		getCoord(acn.curves[1], fbx_time),
+		getCoord(acn.curves[2], fbx_time),
+	}
 }
 
 func (acn *AnimationCurveNode) getBone() *Object {
-	return nil
+	return acn.bone
 }
-
-struct AnimationCurveImpl : AnimationCurve
-{
-	AnimationCurveImpl(const Scene& _scene, const IElement& _element)
-		: AnimationCurve(_scene, _element)
-	{
-	}
-
-	int getKeyCount() const override { return (int)times.size(); }
-	const int64* getKeyTime() const override { return &times[0]; }
-	const float* getKeyValue() const override { return &values[0]; }
-
-	std::vector<int64> times;
-	std::vector<float> values;
-	Type getType() const override { return Type::ANIMATION_CURVE; }
-};
-
-
-struct AnimationCurveNodeImpl : AnimationCurveNode
-{
-	AnimationCurveNodeImpl(const Scene& _scene, const IElement& _element)
-		: AnimationCurveNode(_scene, _element)
-	{
-	}
-
-
-	const Object* getBone() const override
-	{
-		return bone;
-	}
-
-
-	Vec3 getNodeLocalTransform(double time) const override
-	{
-		int64 fbx_time = secondsToFbxTime(time);
-
-		auto getCoord = [](const Curve& curve, int64 fbx_time) {
-			if (!curve.curve) return 0.0f;
-
-			const int64* times = curve.curve.getKeyTime();
-			const float* values = curve.curve.getKeyValue();
-			int count = curve.curve.getKeyCount();
-
-			if (fbx_time < times[0]) fbx_time = times[0];
-			if (fbx_time > times[count - 1]) fbx_time = times[count - 1];
-			for (int i = 1; i < count; ++i)
-			{
-				if (times[i] >= fbx_time)
-				{
-					float t = float(double(fbx_time - times[i - 1]) / double(times[i] - times[i - 1]));
-					return values[i - 1] * (1 - t) + values[i] * t;
-				}
-			}
-			return values[0];
-		};
-
-		return {getCoord(curves[0], fbx_time), getCoord(curves[1], fbx_time), getCoord(curves[2], fbx_time)};
-	}
-
-
-	struct Curve
-	{
-		const AnimationCurve* curve = nullptr;
-		const Scene::Connection* connection = nullptr;
-	};
-
-
-	Curve curves[3];
-	Object* bone = nullptr;
-	DataView bone_link_property;
-	Type getType() const override { return Type::ANIMATION_CURVE_NODE; }
-	enum Mode
-	{
-		TRANSLATION,
-		ROTATION,
-		SCALE
-	} mode = TRANSLATION;
-};
