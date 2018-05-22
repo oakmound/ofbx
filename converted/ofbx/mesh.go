97fb8e94a6f8cab58d263b6853b524f26b17a291
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-05-21 19:14:17 -0500

    Fix syntax errors in mesh

--- converted/ofbx/mesh.go
+++ converted/ofbx/mesh.go
@@ -2,19 +2,16 @@ package ofbx
 
 type Mesh struct {
 	Object
-	geometry *Geometry
-	scene *Scene
+	geometry  *Geometry
+	scene     *Scene
 	materials []*Material
 }
 
-// MeshImpl(const Scene& _scene, const IElement& _element)
-// : Mesh(_scene, _element)
-// , scene(_scene)
-// {
-// is_node = true
-// }
 func NewMesh(scene *Scene, element *IElement) *Mesh {
-	return &Mesh{}
+	m := &Mesh{}
+	m.Object = NewObject(scene, element)
+	m.Object.is_node = true
+	return m
 }
 
 func (m *Mesh) Type() Type {
@@ -26,15 +23,15 @@ func (m *Mesh) getGeometry() *Geometry {
 }
 
 func (m *Mesh) getGeometricMatrix() Matrix {
-	translation := resolveVec3Property(*m, "GeometricTranslation", {0, 0, 0})
-	rotation := resolveVec3Property(*m, "GeometricRotation", {0, 0, 0})
-	scale := resolveVec3Property(*m, "GeometricScaling", {1, 1, 1})
+	translation := resolveVec3Property(*m, "GeometricTranslation", &Vec3{0, 0, 0})
+	rotation := resolveVec3Property(*m, "GeometricRotation", &Vec3{0, 0, 0})
+	scale := resolveVec3Property(*m, "GeometricScaling", &Vec3{1, 1, 1})
 
 	scale_mtx := m.makeIdentity()
-	scale_mtx.m[0] = (float32)scale.x
-	scale_mtx.m[5] = (float32)scale.y
-	scale_mtx.m[10] = (float32)scale.z
-	mtx := m.getRotationMatrix(rotation, RotationOrder::EULER_XYZ)
+	scale_mtx.m[0] = float32(scale.x)
+	scale_mtx.m[5] = float32(scale.y)
+	scale_mtx.m[10] = float32(scale.z)
+	mtx := m.getRotationMatrix(rotation, EULER_XYZ)
 	setTranslation(translation, &mtx)
 
 	return scale_mtx.Mul(mtx)
