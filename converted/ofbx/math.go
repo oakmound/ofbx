8b991173c650c804c764da1c941b7626cd079da0
cyreal123@gmail.com <cyreal123@gmail.com>
2018-05-21 20:18:56 -0400

    Math update

--- converted/ofbx/math.go
+++ converted/ofbx/math.go
@@ -47,7 +47,7 @@ type Quat struct {
 
 
 func (v *Vec3) Minus() *Vec3{
-	return {-v.X, -v.Y, -v.Z}
+	return Vec3{-v.X, -v.Y, -v.Z}
 }
 
 func (v *Vec3) Mul(f){
@@ -61,83 +61,58 @@ func (v *Vec3) Add(v2 *Vec3){
 
 
 func (m1 *Matrix) Mul(m2){
-	Matrix result = make{[]float64, 16}
-	for (int j = 0; j < 4; ++j)
-	{
-		for (int i = 0; i < 4; ++i)
-		{
-			double tmp = 0
-			for (int k = 0; k < 4; ++k)
-			{
+	result := make{[]float64, 16}
+	for j := 0; j < 4; j++	{
+		for  i := 0; i < 4; i++{
+			tmp := 0.0
+			for k := 0; k < 4; k++{
 				tmp += m1.m[i + k * 4] * m2.m[k + j * 4]
 			}
 			res.m[i + j * 4] = tmp
 		}
 	}
-	return res;
+	return res
 }
 
-func makeIdentity() Matrix{
+func makeIdentity() Matrix {  
 	return Matrix{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}	
 }
 
 func (m *Matrix) rotationX(angle float64){
-	Matrix m = makeIdentity();
+	m2 := makeIdentity()
 	//radian
 	c := math.Cos(angle)
 	s := math.Sin(angle)
-	m.m[5] = m.m[10] = c
-	m.m[9] = -s
-	m.m[6] = s
-	return m
+	m2.m[5] = c
+	m2.m[10] = c
+	 m2.m[9] = -s
+	m2.m[6] = s
+	return m2
 }
 
 func (m *Matrix) rotationY(angle float64){
-	Matrix m = makeIdentity()
+	m2 := makeIdentity()
 	//radian
 	c := math.Cos(angle)
 	s := math.Sin(angle)
-	m.m[0] = m.m[10] = c
-	m.m[8] = s
-	m.m[2] = -s
-	return m
+	m2.m[0] = c
+	m2.m[10] = c
+	m2.m[8] = s
+	m2.m[2] = -s
+	return m2
 }
 
 func (m *Matrix) rotationZ(angle float64){
-	Matrix m = makeIdentity()
+	m2 := makeIdentity()
 	//radian
 	c := math.Cos(angle)
 	s := math.Sin(angle)
-	m.m[0] = m.m[5] = c
-	m.m[4] = -s
-	m.m[1] = s
-	return m
+	m2.m[0] =c
+	m2.m[5] = c
+	m2.m[4] = -s
+	m2.m[1] = s
+	return m2
 }
-
-func getRotationMatrix(euler *Vec3, order RotationOrder) Matrix{
-	float64 TO_RAD =  3.1415926535897932384626433832795028 / 180.0 //TODO: Update this
-	rx = rotationX(euler.X * TO_RAD)
-	ry = rotationY(euler.Y * TO_RAD)
-	rz = rotationZ(euler.Z * TO_RAD)
-	switch (order) {
-	default:
-	case RotationOrder::SPHERIC_XYZ:
-		assert(false);
-	case RotationOrder::EULER_XYZ:
-		return rz.Mul(ry).Mul(rx)
-	case RotationOrder::EULER_XZY:
-		return ry.Mul(rz).Mul(rx)
-	case RotationOrder::EULER_YXZ:
-		return rz.Mul(rx).Mul( ry)
-	case RotationOrder::EULER_YZX:
-		return rx.Mul(rz).Mul(ry)
-	case RotationOrder::EULER_ZXY:
-		return ry.Mul(rx).Mul(rz)
-	case RotationOrder::EULER_ZYX:
-		return rx.Mul(ry).Mul(rz)
-}
-
-
 
 func getTriCountFromPoly(indices []int, idx int) (int, int){
 	count := 1
@@ -146,4 +121,32 @@ func getTriCountFromPoly(indices []int, idx int) (int, int){
 	}
 	return count, idx
 }
+
+
+func getRotationMatrix(euler *Vec3, order RotationOrder) Matrix{
+	TO_RAD :=  3.1415926535897932384626433832795028 / 180.0 //TODO: Update this
+	rx = rotationX(euler.X * TO_RAD)
+	ry = rotationY(euler.Y * TO_RAD)
+	rz = rotationZ(euler.Z * TO_RAD)
+	switch (order) {
+	default:
+	case SPHERIC_XYZ:
+		errors.NewError("This should not happen")
+	case EULER_XYZ:
+		return rz.Mul(ry).Mul(rx)
+	case EULER_XZY:
+		return ry.Mul(rz).Mul(rx)
+	case EULER_YXZ:
+		return rz.Mul(rx).Mul( ry)
+	case EULER_YZX:
+		return rx.Mul(rz).Mul(ry)
+	case EULER_ZXY:
+		return ry.Mul(rx).Mul(rz)
+	case EULER_ZYX:
+		return rx.Mul(ry).Mul(rz)
+}
+
+}
+
+
 
