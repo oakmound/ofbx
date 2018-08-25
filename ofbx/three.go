1b00b263ec537703607508f5a81739252e6966fe
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-08-25 12:08:51 -0500

    Add parseParameters, parseDeformers

--- ofbx/three.go
+++ ofbx/three.go
@@ -20,6 +20,9 @@ func (l *Loader) Load(r io.Reader, textureDir string) (*Scene, error) {
 	if err != nil {
 		return err
 	}
+	if l.fbxTree.Objects.LayeredTexture != nil {
+		fmt.Println("layered textures are not supported. Discarding all but first layer.")
+	}
 	return l.parseTree(textureDir)
 }
 
@@ -56,16 +59,9 @@ func NewParsedConnections() ParsedConnections {
 	return ParsedConnections(make(map[int64]ConnectionSet))
 }
 
-type Relationship int 
-
-const (
-	Parent Relationship = iota
-	Child Relationship = iota
-)
-
 type Connection struct {
 	To, From int64
-	Relationship 
+	Relationship string
 }
 
 func (l *Loader) parseConnections() ParsedConnections {
@@ -176,6 +172,169 @@ func (l *Loader) loadTexture(tx TextureNode, images map[int64]io.Reader) (io.Rea
 		return nil, errors.New("Expected texture node child")
 	}
 	return images[cns[0].ID], nil
+}
+
+type Color struct {
+	R, G, B float32
+}
+
+type MaterialNode struct {
+	BumpFactor *float64
+	Diffuse *[3]float32
+	DiffuseColor *[3]float32
+	DisplacementFactor *float64
+	Emissive *[3]float32
+	EmissiveColor *[3]float32
+	EmissiveFactor *float64
+	Opacity *float64
+	ReflectionFactor *float64
+	Shininess *float64
+	Specular *[3]float32
+	SpecularColor *[3]float32
+}
+
+type MaterialParameters struct {
+	BumpFactor float64
+	Diffuse Color
+	DisplacementFactor float64
+	Emissive Color
+	EmissiveFactor float64
+	Opacity float64
+	ReflectionFactor float64
+	Shininess float64
+	Specular Color
+	Transparent bool
+
+	bumpMap Texture
+	normalMap Texture
+	specularMap Texture
+	emissiveMap Texture
+	diffuseMap Texture
+	alphaMap Texture
+	displacementMap Texture
+	envMap Texture
+} 
+
+func (l *Loader) parseParameters(mn MaterialNode, txs map[int64]Texture, id int64) MaterialParameters {
+	parameters := MaterialParameters{}
+
+	if materialNode.BumpFactor != nil  {
+		parameters.BumpFactor = *materialNode.BumpFactor
+	}
+	if materialNode.Diffuse != nil  {
+		parameters.Diffuse.R = (*MaterialNode.Diffuse)[0]
+		parameters.Diffuse.G = (*MaterialNode.Diffuse)[1]
+		parameters.Diffuse.B = (*MaterialNode.Diffuse)[2]
+	} else if materialNode.DiffuseColor != nil  {
+		// The blender exporter exports diffuse here instead of in materialNode.Diffuse
+		parameters.Diffuse.R = (*MaterialNode.DiffuseColor)[0]
+		parameters.Diffuse.G = (*MaterialNode.DiffuseColor)[1]
+		parameters.Diffuse.B = (*MaterialNode.DiffuseColor)[2]
+	}
+	if materialNode.DisplacementFactor != nil  {
+		parameters.displacementScale = *materialNode.DisplacementFactor;
+	}
+	if materialNode.Emissive != nil  {
+		parameters.Emissive.R = (*MaterialNode.Emissive)[0]
+		parameters.Emissive.G = (*MaterialNode.Emissive)[1]
+		parameters.Emissive.B = (*MaterialNode.Emissive)[2]
+	} else if materialNode.EmissiveColor != nil  {
+		// The blender exporter exports emissive color here instead of in materialNode.Emissive
+		parameters.Emissive.R = (*MaterialNode.EmissiveColor)[0]
+		parameters.Emissive.G = (*MaterialNode.EmissiveColor)[1]
+		parameters.Emissive.B = (*MaterialNode.EmissiveColor)[2]
+	}
+	if materialNode.EmissiveFactor != nil  {
+		parameters.EmissiveFactor = *materialNode.EmissiveFactor
+	}
+	if materialNode.Opacity != nil  {
+		parameters.Opacity = *materialNode.Opacity
+	}
+	if parameters.opacity < 1.0 != nil  {
+		parameters.Transparent = true;
+	}
+	if materialNode.ReflectionFactor != nil  {
+		parameters.ReflectionFactor = *materialNode.ReflectionFactor;
+	}
+	if materialNode.Shininess != nil  {
+		parameters.Shininess = *materialNode.Shininess;
+	}
+	if materialNode.Specular != nil  {
+		parameters.Specular.R = (*MaterialNode.Specular)[0]
+		parameters.Specular.G = (*MaterialNode.Specular)[1]
+		parameters.Specular.B = (*MaterialNode.Specular)[2]
+	} else if materialNode.SpecularColor != nil  {
+		// The blender exporter exports specular color here instead of in materialNode.Specular
+		parameters.Specular.R = (*MaterialNode.SpecularColor)[0]
+		parameters.Specular.G = (*MaterialNode.SpecularColor)[1]
+		parameters.Specular.B = (*MaterialNode.SpecularColor)[2]
+	}
+
+	for _, child := range l.connections[id].children {
+		// TODO: Remember to throw away layered things and use the first layer's
+		// ID for layered textures
+		txt := txs[child.id]
+		switch child.Relationship  {
+			case "Bump":
+				parameters.bumpMap = txt
+			case "DiffuseColor":
+				parameters.diffuseMap = txt
+			case "DisplacementColor":
+				parameters.displacementMap = txt
+			case "EmissiveColor":
+				parameters.emissiveMap = txt
+			case "NormalMap":
+				parameters.normalMap = txt
+			case "ReflectionColor":
+				parameters.envMap = txt
+				parameters.envMap.mapping = EquirectangularReflectionMapping;
+			case "SpecularColor":
+				parameters.specularMap = txt
+			case "TransparentColor":
+				parameters.alphaMap = txt
+				parameters.transparent = true
+			//case "AmbientColor":
+			//case "ShininessExponent": // AKA glossiness map
+			//case "SpecularFactor": // AKA specularLevel
+			//case "VectorDisplacementColor": // NOTE: Seems to be a copy of DisplacementColor
+			default:
+				fmt.Printf("%s map is not supported in three.js, skipping texture.\n", child.Relationship )
+		}
+	}
+	return parameters
+}
+
+type Skeleton struct {
+	ID int64
+}
+type MorphTarget struct {
+	ID int64
+}
+
+func (l *Loader) parseDeformers() (map[int64]Skeleton, map[int64]MorphTarget) {
+	skeletons := make(map[int64]Skeleton)
+	morphTargets := make(map[int64]MorphTarget)
+	for id, dn := range l.fbxTree.Objects.Deformer {
+		relationships := l.connections[id]
+		if dn.attrType == "Skin" {
+			skel := l.parseSkeleton(relationships, dn)
+			skel.ID = id
+			if len(relationships.parents) > 1 {
+				fmt.Println("skeleton attached to more than one geometry is not supported.")
+			}
+			skel.geometryID = relationships.parents[0].ID
+			skeletons[id] = skel
+		} else if dn.attrType == "BlendShape" {
+			mt := MorphTarget{}
+			mt.ID = id
+			mt.rawTargets = l.parseMorphTargets(relationships, l.fbxTree.Objects.Deformer)
+			if len(relationships.parents) > 1 {
+				fmt.Println("morph target attached to more than one geometry is not supported.")
+			}
+			morphTargets[id] = mt
+		}
+	}
+	return skeletons, morphTargets
 }
 
 func isBinary(r io.Reader) bool {
