f8dd77b72785b5cc248749cf79933dfce0be1eb5
Patrick <patrick.d.stephen@gmail.com>
2018-11-05 20:35:32 -0600

    Move connection to own file

--- threefbx/three.go
+++ threefbx/three.go
@@ -61,62 +61,6 @@ func (l *Loader) parseTree(textureDir string) (Model, error) {
 	return l.parseScene(skeletons, morphTargets, geometry, materials), nil
 }
 
-type ParsedConnections map[int]ConnectionSet
-
-type ConnectionSet struct {
-	parents  []Connection
-	children []Connection
-}
-
-func NewParsedConnections() ParsedConnections {
-	return ParsedConnections(make(map[int]ConnectionSet))
-}
-
-type Connection struct {
-	ID           int
-	To, From     int
-	Relationship string
-}
-
-func NewConnection(n *Node) (Connection, error) {
-	cn := Connection{}
-	var ok bool
-	cn.ID, ok = n.props["ID"].Payload().(int)
-	if !ok {
-		return cn, errors.New("Node lacking Connection properties")
-	}
-	cn.To, ok = n.props["To"].Payload().(int)
-	if !ok {
-		return cn, errors.New("Node lacking Connection properties")
-	}
-	cn.From, ok = n.props["From"].Payload().(int)
-	if !ok {
-		return cn, errors.New("Node lacking Connection properties")
-	}
-	cn.Relationship, ok = n.props["Relationship"].Payload().(string)
-	if !ok {
-		return cn, errors.New("Node lacking Connection properties")
-	}
-	return cn, nil
-}
-
-func (l *Loader) parseConnections() (ParsedConnections, error) {
-	cns := NewParsedConnections()
-	for _, n := range l.tree.Objects["Connections"] {
-		cn, err := NewConnection(n)
-		if err != nil {
-			return nil, err
-		}
-		cf := cns[cn.From]
-		ct := cns[cn.To]
-		cf.parents = append(cf.parents, cn)
-		ct.children = append(ct.children, cn)
-		cns[cn.From] = cf
-		cns[cn.To] = ct
-	}
-	return cns, nil
-}
-
 type VideoNode struct {
 	ContentType string
 	Filename    string
